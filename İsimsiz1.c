8ba851cb6f760d437c3e89e1224b2d6ec9164d6c
omerfaruk749 <45922939+omerfaruk749@users.noreply.github.com>
2018-12-17 14:55:20 +0300

    Create İsimsiz1.c

---
+++ İsimsiz1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main ()
+{
+	
+	
+	int i,j;
+	int yildiz, bosluk;
+	for(i=0;i<20;i++)
+	{
+		
+		for(j=0;j<bosluk;j++)
+			{
+				printf(" ");
+			
+			}
+		for (j=0;j<yildiz;j++)
+			{
+				printf("*");
+			}
+		printf("\n");
+		bosluk++;
+		yildiz+=2;	
+		}
+	
+	
+	
+	
+	
+	
+	
+}
